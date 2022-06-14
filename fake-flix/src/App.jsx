d1e4987f2e59990fb9b7895274177b2ad11760c0
FernandoEtcheverry <etchecuenta@gmail.com>
2022-06-14 13:14:35 -0300

    singlemovie

--- fake-flix/src/App.jsx
+++ fake-flix/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
+import SingleMovie from "./pages/SingleMovie";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<SingleMovie />} />
       </Routes>
     </div>
   );
--- fake-flix/src/pages/SingleMovie.jsx
+++ fake-flix/src/pages/SingleMovie.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function SingleMovie() {
-  return <div>SingleMovie</div>;
+  const [singleMovie, setSingleMovie] = useState([]);
+  const params = useParams();
+
+  useEffect(() => {
+    const getSingleMovie = async () => {
+      try {
+        const response = await axios({
+          method: "get",
+          baseURL: "https://api.themoviedb.org/3/movie/",
+          params: {
+            api_key: "6b7bc1a82647ebf94fa65665cc23fc20",
+            movie_id: `${params.id}`,
+          },
+        });
+        setSingleMovie(response.data.results);
+        console.log(response.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getSingleMovie();
+  }, []);
+
+  return (
+    <div>
+      <h2>Single Movie</h2>
+    </div>
+  );
 }
 
 export default SingleMovie;
