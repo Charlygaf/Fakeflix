13b959662c8e948eb733a5ddb1d80ffd701f3da1
FernandoEtcheverry <etchecuenta@gmail.com>
2022-06-14 13:19:09 -0300

    id

--- fake-flix/src/pages/Home.jsx
+++ fake-flix/src/pages/Home.jsx
@@ -45,7 +45,7 @@ export default function Home() {
         <div className="row">
           {movies.map((movie) => (
             <div className="col-6 col-md-2 rounded mt-4 " key={movie.id}>
-              <Link to="/movie/:id">
+              <Link to={`/movie/${movie.id}`}>
                 <img
                   src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
                   alt={movie.title}
